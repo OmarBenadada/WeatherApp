e503aa25220d95c9ad350d8878ccfe56997bea9b
omae sama <omarkillusama@gmail.com>
2024-12-08 21:13:46 +0100

    addding the Status of the City and Handling the KeyError

--- weather_app.py
+++ weather_app.py
@@ -13,9 +13,9 @@ class WeatherWindow(QWidget):
         self.button.clicked.connect(self.on_click)        
         self.cityname=QLineEdit(self)
         self.cityname.setPlaceholderText("Enter the name...")
-        self.temperarute=QLabel("60°C",self)
-        self.Hala=QLabel("Sunny",self)
-        self.emoji=QLabel("☀",self)
+        self.temperarute=QLabel(self)
+        self.Hala=QLabel(self)
+        self.emoji=QLabel(self)
         self.initui()
         
     def initui(self):
@@ -40,32 +40,33 @@ class WeatherWindow(QWidget):
         self.cityname.setMaximumHeight(60)
         
     def on_click(self):
-        
         base_url = "https://api.openweathermap.org/data/2.5/weather"
         input_text = self.cityname.text()
         para={"q":input_text,
               "appid":"f43e187881ffa1ef9623a04fb490b629",
               "units":"metric"}
         responce=requests.get(base_url,params=para)
-        
-        match responce.status_code:
-            
-            case 100 | 101 | 102 | 103:
-                self.temperarute.setText("Informational Error")
+        try:
+            match responce.status_code:
+                
+                case 100 | 101 | 102 | 103:
+                    self.temperarute.setText("Informational Error")
 
-            case 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226:
-                self.temperarute.setText(str(f'{responce.json()['main']['temp']}°C'))
+                case 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226:
+                    self.temperarute.setText(str(f'{responce.json()['main']['temp']}°C'))
 
-            case 300 | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 308:
-                self.temperarute.setText('Redirection Responses Error')
+                case 300 | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 308:
+                    self.temperarute.setText('Redirection Responses Error')
 
-            case 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 421 | 422 | 423 | 424 | 425 | 426 | 428 | 429 | 431 | 451:
-                self.temperarute.setText('Client Errors')
+                case 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 421 | 422 | 423 | 424 | 425 | 426 | 428 | 429 | 431 | 451:
+                    self.temperarute.setText('Client Errors')
 
-            case 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511:
-                self.temperarute.setText('Server Errors')
-            case _:
-                self.temperarute.setText('Enkown Error')   
+                case 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511:
+                    self.temperarute.setText('Server Errors')
+                case _:
+                    self.temperarute.setText('Enkown Error')
+        except KeyError:
+                print("Pls Try To Re-Write the City Name")
         try:         
             match responce.json()['weather'][0]["main"]:
                 case "Clear":
@@ -102,6 +103,10 @@ class WeatherWindow(QWidget):
                     return "Unknown Weather Status ❓"      
         except KeyError:
                 print("pls Try to correct the name you entered")
+        try:
+            self.Hala.setText(responce.json()['weather'][0]["main"])
+        except KeyError:
+            self.temperarute.setText("Pls try to Re-Write the City name")
            
 class main():
     weatherapp=QApplication(sys.argv)
